d755ecfa3448aefd8e38f30ff04fa449c377dc24
Nastya Orlova <web.orlova.dev@gmail.com>
2025-09-04 13:28:16 +0300

    feat: Add static files middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import path from 'path';
 import productRoutes from './routes/productRoutes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/weblarek')
