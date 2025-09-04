7a330c78dc932c057f5d0e357e412c1dfb043297
Nastya Orlova <web.orlova.dev@gmail.com>
2025-09-04 10:46:45 +0300

    chore: Add MongoDB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/weblarek')
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Подключились к MongoDB');
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error('Ошибка подключения к MongoDB:', error);
+  });
 
 app.get('/', (_req, res) => {
   res.send('ку');
