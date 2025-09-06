f28b479388f01472f7a868b95d9854c8120b4b2a
Nastya Orlova <web.orlova.dev@gmail.com>
2025-09-06 17:57:52 +0300

    feat: Add MongoDB connection, CORS config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,29 @@
 import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
+app.use(express.json());
 
-app.use('/', (_req, res) => {
-  res.send('ку');
-});
+mongoose
+  .connect('mongodb://127.0.0.1:27017/weblarek')
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Подключились к MongoDB');
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error('Ошибка подключения к MongoDB:', error);
+  });
+
+app.use(routes);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
